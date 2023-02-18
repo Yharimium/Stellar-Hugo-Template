4d5139dcec5e676d38b8d5475efef78f6ea37d65
cyolc932 <cyolc932@users.noreply.github.com>
2023-02-18 11:20:45 +0000

    deploy: 8f3a258a3b88096470af979e8ccde58cb5fdb437

---
+++ js/toctrack.js
@@ -0,0 +1,48 @@
+$anchor = $("#TableOfContents a");
+$widgets = $(".widgets");
+$heading = $(".content").children("h2, h3, h4");
+
+onClick = false;
+
+scrollParentToChild = (parent, child) => {
+    if (!parent || !child)
+        return;
+    var parentRect = parent.getBoundingClientRect();
+    var childRect = child.getBoundingClientRect();
+    var isViewable = (childRect.top >= parentRect.top) && (childRect.bottom <= parentRect.top + parent.clientHeight);
+    if (!isViewable) {
+        const scrollTop = childRect.top - parentRect.top;
+        const scrollBot = childRect.bottom - parentRect.bottom;
+        parent.scrollTop += (scrollTop + scrollBot) / 2;
+    }
+}
+
+currentAnchor = () => {
+    var heading = $heading.withinviewport();
+    return heading.length && $(`[href=\"${heading[0].id}\"]`);
+}
+
+setActiveAnchor = ($cntAnchor) => {
+    if (!$cntAnchor)
+        return;
+    $anchor.removeClass("active");
+    $cntAnchor.addClass("active");
+    scrollParentToChild($widgets[0], $cntAnchor[0]);
+}
+
+$(window).scroll( () => {
+    onClick ? onClick = false : setActiveAnchor(currentAnchor());
+});
+
+setActiveAnchor(currentAnchor());
+
+$anchor.each( (id, el) => {
+    $(el).click( () => {
+        onClick = true;
+        setActiveAnchor($(el));
+        var heading = $heading.filter(`[id=\"${$(el).attr("href")}\"]`);
+        heading.css({ color: "#f53900" })
+            .animate({ opacity: 1 }, 100)
+            .animate({ color: "#000000" }, "swing");
+    });
+});
